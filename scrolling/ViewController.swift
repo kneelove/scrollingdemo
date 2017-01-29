7770ad539e6ebca8ee9744122103dd4699d73d38
Nilav Vaghasiya <nilav@Nilavs-Mac.local>
2017-01-29 19:37:37 +0530

    Scrolling Achieved Within The View

--- scrolling/ViewController.swift
+++ scrolling/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollview: UIScrollView!
+    var images = [UIImageView()]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    override func viewDidAppear(_ animated: Bool) {
+        var content:CGFloat = 0.0
+        
+        
+        for x in 0...2{
+            
+            let image = UIImage(named: "icon\(x).png")
+            
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            var newX:CGFloat = 0.0
+            newX = view.frame.width/2+view.frame.size.width*CGFloat(x)
+            content += newX
+            scrollview.addSubview(imageView)
+            imageView.frame = CGRect(x: newX-75, y: (view.frame.size.height/2), width: 150, height:150)
+            
+            
 
+             }
+        scrollview.contentSize = CGSize(width: content, height: view.frame.height
+        )
+    
+        
+        
 
 }
 
+}
